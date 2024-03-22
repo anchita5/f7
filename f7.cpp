c260c48098d1254af07dd850330d3797f4a935a5
Anchita Kakria <anchita585@gmail.com>
2024-03-22 18:12:39 -0400

    binary data file writing done

--- f7.cpp
+++ f7.cpp
@@ -8,16 +8,20 @@ int main(void)
 {
     FILE* fptr = NULL; // File pointer for text file
 
+    FILE* fbPtr = NULL;
+
+    const unsigned short kBinaryData[] = { 26946, 24942, 31090, 25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
+
     /*Text file open */
-    if ((fptr = fopen("myTextFile.txt", "w")) != NULL) // Open file and check for error
+    if ((fptr = fopen("myTextFile.txt", "w")) != NULL) 
     {
         int counter = 0;
 
         while (counter < 2)
         {
-            if (fprintf(fptr, "This is line %d\n", counter + 1) < 0) // Write file and check for error simultaneously
+            if (fprintf(fptr, "This is line %d\n", counter + 1) < 0) 
             {
-                printf("Error writing to file"); // If error in writing to file, print error message and exit code
+                printf("Error writing to file"); 
                 return -2;
             }
             counter++;
@@ -25,13 +29,46 @@ int main(void)
 
         if (fclose(fptr) != 0)
         {
-            printf("Error closing file"); // Print error message and exit if file failed to close
+            printf("Error closing file"); 
             return -1;
         }
     }
     else
     {
-        printf("Error opening file"); // Print error message and exit if file failed to open
+        printf("Error opening file"); 
+        return -1;
+    }
+
+
+    /*Writing to binary file functionality */
+
+    if ((fbPtr = fopen("myEvenDataList.data", "wb")) != NULL)
+    {
+        int lengthOfArray = sizeof(kBinaryData) / sizeof(kBinaryData[0]);
+        int counter = 0;
+
+        while (counter < lengthOfArray)
+        {
+            if (kBinaryData[counter] % 2 == 0)
+            {
+                if ((fwrite(&kBinaryData[counter], sizeof(unsigned short), 1, fbPtr)) < 1)
+                {
+                    printf("Error writing to file");
+                    return -1;
+                }
+            }
+            counter++;
+        }
+
+        if (fclose(fbPtr) != 0)
+        {
+            printf("Error closing file");
+            return -1;
+        }
+    }
+    else
+    {
+        printf("Error opening binary file");
         return -1;
     }
 
